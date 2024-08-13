4ac7813c857f6ffb86f18bb90ee9eebb4ed0026b
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-13 15:06:12 +0530

    Navbar file cre

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,11 +5,21 @@ import Contact from './Components/Contact';
 import Loginform from './Components/Loginform';
 import Signup from './Components/Signup';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/privacy' element={<Privacy/>} />
+        <Route path='/loginform' element={<Loginform/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/signup' element={<Signup/>} />
+      </Routes>
+    </Router>
+  </div>
 }
 
 export default App
