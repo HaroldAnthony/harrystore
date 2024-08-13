92c75f84d9d09e6da8a2129cb28b7efe9af06ba2
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-13 14:19:38 +0530

    imports in App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Home from './Components/Home';
+import Privacy from './Components/Privacy';
+import Contact from './Components/Contact';
+import Loginform from './Components/Loginform';
+import Signup from './Components/Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
