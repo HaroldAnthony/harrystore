e948b8e1c1f14b14f1789566ef7a934991dd129b
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-14 13:45:59 +0530

    menu icon

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import HSLogo from "./images/hs-logo.png";
 import "./Navbar.css";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
   return (
@@ -10,56 +11,59 @@ const Navbar = () => {
       <div className="navbar_div">
         <div>
           <input type="text" placeholder="Search here" />
-          {/* SearchIcon */}
+          <SearchIcon className="Search_icon_input" />
           {/* CloseIcon */}
         </div>
         <div>
           <div className="navbar_main">
             <div>
-              <div>{/* SearchIcon */}</div>
-              {/* MenuIcon */}
+              <div>
+                {" "}
+                <SearchIcon className="Search_icon" />
+              </div>
+              <MenuIcon className="menu_icon" />
             </div>
             <div>
               <img src={HSLogo} className="heading_logo" alt="" />
             </div>
             <div className="bad_search">
-              {/* SearchIcon */}
+              <SearchIcon className="Search_icon2" />
               {/* LocalIcon */}
             </div>
           </div>
           <div className="Nav_links">
-          <Link to="/" className="link" >
-          Home
-          </Link>
-          <Link to="/contact" className="link" >
-          Contact
-          </Link>
-          <Link to="/privacy" className="link" >
-          Privacy
-          </Link>
-          <Link to="/loginform" className="link" >
-          Login
-          </Link>
-          <Link to="/signup" className="link" >
-          Signup
-          </Link>
+            <Link to="/" className="link">
+              Home
+            </Link>
+            <Link to="/contact" className="link">
+              Contact
+            </Link>
+            <Link to="/privacy" className="link">
+              Privacy
+            </Link>
+            <Link to="/loginform" className="link">
+              Login
+            </Link>
+            <Link to="/signup" className="link">
+              Signup
+            </Link>
           </div>
         </div>
         <Link to="/" className="sidebar_link">
           Home
-          </Link>
-          <Link to="/contact" className="sidebar_link">
+        </Link>
+        <Link to="/contact" className="sidebar_link">
           Contact
-          </Link>
-          <Link to="/privacy" className="sidebar_link">
+        </Link>
+        <Link to="/privacy" className="sidebar_link">
           Privacy
-          </Link>
-          <Link to="/loginform" className="sidebar_link">
+        </Link>
+        <Link to="/loginform" className="sidebar_link">
           Login
-          </Link>
-          <Link to="/signup" className="sidebar_link">
+        </Link>
+        <Link to="/signup" className="sidebar_link">
           Signup
-          </Link>
+        </Link>
       </div>
     </div>
   );
