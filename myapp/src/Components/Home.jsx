6c97daafe475351853fb6e4083e591a6f2c74f17
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-14 15:47:03 +0530

    home jsx edit

--- myapp/src/Components/Home.jsx
+++ myapp/src/Components/Home.jsx
@@ -5,15 +5,17 @@ const Home = () => {
   return (
     <div>
       <div className="home_header">
-        <div className="header_container">
-          <h1>Hacker-for-hire</h1>
-          <p>
-            With ample skills and experience, non-deepweb hackers are ready to
-            perform all kinds of hacking services, and efficiently deliver
-            results to serious customers, who co-operate with our modus
-            operandi.
-          </p>
-          <button>Contact us</button>
+        <div className="data">
+          <div className="header_container">
+            <h1>Hacker-for-hire</h1>
+            <p>
+              With ample skills and experience, non-deepweb hackers are ready to
+              perform all kinds of hacking services, and efficiently deliver
+              results to serious customers, who co-operate with our modus
+              operandi.
+            </p>
+            <button className="Shampoobtn">Contact us</button>
+          </div>
         </div>
       </div>
     </div>
