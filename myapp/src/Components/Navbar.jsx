706893480a4bd8f8f855c73ca8704491d53a5bfb
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-14 12:59:18 +0530

    Navbar js file edit

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import HSLogo from "./images/hs-logo.png";
 
 const Navbar = () => {
   return (
     <div>
-      <div className='navbar_div'>
+      <div className="navbar_div">
         <div>
-          <input type='text' placeholder='Search here' />
+          <input type="text" placeholder="Search here" />
           {/* SearchIcon */}
           {/* CloseIcon */}
         </div>
         <div>
-          <div className='navbar_main'>
+          <div className="navbar_main">
             <div>
-              <div>
-                {/* SearchIcon */}
-              </div>
+              <div>{/* SearchIcon */}</div>
               {/* MenuIcon */}
             </div>
             <div>
-              <img src='' alt='' />
+              <img src={HSLogo} className="heading_logo" alt="" />
+            </div>
+            <div className="bad_search">
+              {/* SearchIcon */}
+              {/* LocalIcon */}
             </div>
           </div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/privacy">Privacy</Link>
+          <Link to="/loginform">Login</Link>
+          <Link to="/signup">Signup</Link>
         </div>
       </div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/privacy">Privacy</Link>
-        <Link to="/loginform">Login</Link>
-        <Link to="/signup">Signup</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
