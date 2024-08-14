d14851cdca385d409455dea9fc20f9dea2eb78d6
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-14 15:27:51 +0530

    home jsx content

--- myapp/src/Components/Home.jsx
+++ myapp/src/Components/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return <div>
-    <div className='home_header'>
-      
+  return (
+    <div>
+      <div className="home_header">
+        <div className="header_container">
+          <h1>Hacker-for-hire</h1>
+          <p>
+            With ample skills and experience, non-deepweb hackers are ready to
+            perform all kinds of hacking services, and efficiently deliver
+            results to serious customers, who co-operate with our modus
+            operandi.
+          </p>
+        </div>
+      </div>
     </div>
-    </div>;
-
+  );
 };
 
-export default Home
+export default Home;
