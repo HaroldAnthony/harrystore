e031517e1d65c21aede214398ae1513f213a37e8
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-14 11:57:22 +0530

    fc 14-08-24

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
+      <div className='navbar_div'>
+        <div>
+          <input type='text' placeholder='Search here' />
+        </div>
+      </div>
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/privacy">Privacy</Link>
