d3083f7cf9a482e1a085ace4397bf5d4a361b508
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-20 16:01:24 +0530

    Amazon file add

--- myapp/src/Components/Cartform.js
+++ myapp/src/Components/Cartform.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Cart components/Navbar';
-
+import Amazon from './Cart components/Amazon';
 
 const Cartform = () => {
   return (
     <div>
         <Navbar/>
+        <Amazon/>
         
     </div>
   );
