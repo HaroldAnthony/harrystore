38b6acfa1529c01baefb9b94346dca698bf13e25
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-09-12 12:46:57 +0530

    fc 12-09-24

--- myapp/src/Components/Cart components/Navbar.jsx
+++ myapp/src/Components/Cart components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import "../styles/navbar.css"
+import "../styles/navbar.css";
 
-const Navbar = ({setShow, size}) => {
+const Navbar = ({ setShow, size }) => {
   return (
     <nav>
       <div className="nav_box">
-        <span className="my_shop" onClick={() => setShow(true)}>My shop</span>
-        <div className="cart" onClick={ () => setShow(false)}>
+        <span className="my_shop" onClick={() => setShow(true)}>
+          My shop
+        </span>
+        <div className="cart" onClick={() => setShow(false)}>
           <span>
             <ShoppingCartIcon />
           </span>
