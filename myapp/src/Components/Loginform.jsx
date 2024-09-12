4b324fa83f292e30b1b206a5e854a0979af7a2d6
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-09-12 12:44:51 +0530

    login section d

--- myapp/src/Components/Loginform.jsx
+++ myapp/src/Components/Loginform.jsx
@@ -3,39 +3,61 @@ import { useState } from "react";
 import "./Loginup.css";
 
 const account1 = {
-  username: "Harold",
+  username: "harry",
   pin: 1111,
 };
 const account2 = {
-  username: "Henry",
+  username: "henry",
   pin: 2222,
 };
 const account3 = {
-  username: "Vicky",
+  username: "vicky",
   pin: 3333,
 };
 const account4 = {
-  username: "Joe",
+  username: "joe",
   pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
 
 const Loginform = () => {
-  const [user, setuser] = useState("");
+  const [username, setusername] = useState("");
   const [pin, setpin] = useState("");
   const [error, seterror] = useState("");
 
+  const handlesubmit = (event) => {
+    event.preventDefault();
+    const matchingaccount = accounts.find(
+      (account) => account.username === username && account.pin === Number(pin)
+    );
+    if (matchingaccount) {
+      seterror("You are successfully logged in!");
+    } else {
+      seterror("Incorrect username or pin");
+    }
+  };
+
+  const valuehandler = (event) => {
+    // console.log(event);
+    setusername(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <div>
-      <form className="sign-in-form" action="">
+      <form onSubmit={handlesubmit} className="sign-in-form" action="">
         <label>
           Username
-          <input type="text" value={username} />
+          <input type="text" value={username} onChange={valuehandler} />
         </label>
         <label>
           Password
-          <input type="text" value={pin} />
+          <input
+            type="text"
+            value={pin}
+            onChange={(event) => setpin(event.target.value)}
+          />
         </label>
         <br />
         <button className="loginbtn" type="submit">
